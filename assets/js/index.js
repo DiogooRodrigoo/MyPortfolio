863fb6c5a30d770b926ea644f006c41ac8ba52de
Diogo Rodrigo <diogorodrigo@MacBook-Air-de-Diogo.local>
2024-05-31 19:49:09 -0300

    feature: development of the footer and scrollup component

--- assets/js/index.js
+++ assets/js/index.js
@@ -20,5 +20,11 @@ modalClose.forEach((modalClose) => {
     });
   });
 });
-/*---=== SWIPER TESTIMONIAL ===---*/
+
 /*---=== SHOW SCROLL UP ===---*/
+const scrollUp = () =>{
+	const scrollUp = document.getElementById('scroll-up')
+	this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+						: scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
